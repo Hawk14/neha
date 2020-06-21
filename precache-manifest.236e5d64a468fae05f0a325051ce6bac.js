de44be7a77d91bc80482c328863b69a5c6a1dc79
Hawk14 <neha.gemini92@gmail.com>
2020-06-21 23:13:13 +0530

    Updates

---
+++ precache-manifest.236e5d64a468fae05f0a325051ce6bac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eff94ceb7a5564c7e9c1a1447b68c328",
+    "url": "/neha/index.html"
+  },
+  {
+    "revision": "3e3fa876fd364c9aa670",
+    "url": "/neha/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bf79e0fc48d59fd198f7",
+    "url": "/neha/static/js/2.4c736102.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/neha/static/js/2.4c736102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e3fa876fd364c9aa670",
+    "url": "/neha/static/js/main.08d4557d.chunk.js"
+  },
+  {
+    "revision": "12b48262b91b2b3dff7d",
+    "url": "/neha/static/js/runtime-main.67ddc4e6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/neha/static/media/logo.5d5d9eef.svg"
+  }
+]);
